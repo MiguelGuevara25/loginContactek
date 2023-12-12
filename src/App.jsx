89c877904a5fdf7d281995805af98b72faa6de5d
Miguel Guevara <miguel_g1789@hotmail.com>
2023-12-12 17:23:55 -0500

    tickets puestos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
+import {
+  FaFolderOpen,
+  FaUser,
+  FaPhoneAlt,
+  FaWhatsapp,
+  FaEnvelope,
+  FaTag,
+  FaAddressCard,
+} from "react-icons/fa";
+import { BsPinFill } from "react-icons/bs";
 import axios from "axios";
 
 function App() {
@@ -10,7 +20,8 @@ function App() {
         "https://bio.contactek.com/bio/api_listado_base/"
       );
       const data = response.data;
-      setTickets(data);
+      // console.log(data);
+      setTickets(data[0].tickets);
     } catch (error) {
       console.log(error);
     }
@@ -21,15 +32,69 @@ function App() {
   }, []);
 
   return (
-    <div className="flex">
-      <div className="w-1/4 h-40 bg-red-500">
-        {tickets.map((info) => {
-          info.tickets.map((ticket) => {
-            console.log(ticket);
-          });
+    <div className="flex w-[95%] mx-auto">
+      <div className="w-1/4 h-40 bg-red-500"></div>
+      <div className="w-3/4">
+        {tickets.map((ticket) => {
+          // console.log(ticket);
+          return (
+            <div
+              key={ticket.id}
+              className="flex px-12 py-3 mb-[1px] items-center rounded-2xl justify-between"
+              style={{ backgroundColor: ticket.color }}
+            >
+              <div className="flex-1">
+                <div className="flex">
+                  <span>{ticket.id}</span>
+                  <span className="flex items-center">
+                    <FaFolderOpen />
+                    Abrir
+                  </span>
+                </div>
+
+                <span className="flex items-center">
+                  <FaUser /> {ticket.nombre_gestor}
+                </span>
+              </div>
+
+              <div className="flex flex-col flex-1">
+                <span className="flex items-center">
+                  <FaTag /> {ticket.nombre_tipologia1}
+                </span>
+                <span>{ticket.Estado}</span>
+              </div>
+
+              <div className="flex flex-col flex-1">
+                <span className="flex items-center">
+                  <FaAddressCard /> {ticket.nombre_servicio}
+                </span>
+                <span>
+                  Creada el: <span>{ticket.fecha_inicio}</span>
+                </span>
+              </div>
+
+              <div className="flex flex-col flex-1">
+                <span className="flex items-center">
+                  <BsPinFill /> {ticket.nombre_tipologia2}
+                </span>
+                <span>
+                  Creada el: <span>{ticket.fecha_actualizado}</span>
+                </span>
+              </div>
+
+              <div className="flex flex-1 items-center justify-end gap-1">
+                {ticket.canal_phone === "1" && (
+                  <FaPhoneAlt className="text-2xl" />
+                )}
+                {ticket.canal_ws === "1" && <FaWhatsapp className="text-2xl" />}
+                {ticket.canal_mail === "1" && (
+                  <FaEnvelope className="text-2xl" />
+                )}
+              </div>
+            </div>
+          );
         })}
       </div>
-      <div className="w-3/4 h-40 bg-green-500"></div>
     </div>
   );
 }
